99a9b171c7e2a73e9ae829020c72a0e51e8392ed
seyiooo <zeyon.dany@gmail.com>
2026-03-23 09:03:24 +0100

    style: replace default titles by custom ones

--- src/renderer/components/sidebar.js
+++ src/renderer/components/sidebar.js
@@ -1,12 +1,17 @@
 export default class Sidebar {
     #btn;
-
+ 
     constructor(onViewCurrent) {
         this.#btn = document.getElementById('btn-current');
-
+ 
         this.#btn.addEventListener('click', () => onViewCurrent());
+ 
+        document.querySelectorAll('.sidebar button[title]').forEach((btn) => {
+            btn.dataset.tooltip = btn.title;
+            btn.removeAttribute('title');
+        });
     };
-
+ 
     setActive(active) {
         this.#btn.classList.toggle('active', active);
     };
