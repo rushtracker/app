c3c1b85110848d55ccb86ca5e940e5742abed529
seyiooo <zeyon.dany@gmail.com>
2026-04-02 05:45:46 +0200

    fix: forgot error logger function

--- src/classes/Logger.js
+++ src/classes/Logger.js
@@ -7,11 +7,21 @@ module.exports = class Logger {
     };
   }
 
-  log(message, date = new Date()) {
-    const timestamp = `[${date.toLocaleDateString('fr-FR').replaceAll('/', '-')} ${date.toLocaleTimeString('fr-FR')}]`.gray;
+  get #timestamp() {
+    const date = new Date();
 
-    console.log(`${timestamp} ${message}`);
+    return `[${date.toLocaleDateString('fr-FR').replaceAll('/', '-')} ${date.toLocaleTimeString('fr-FR')}]`.gray;
+  }
+
+  log(message) {
+    console.log(`${this.#timestamp} ${message}`);
 
     return true;
+  }
+
+  error(error) {
+    console.error(`${this.#timestamp} ${'[ERROR]'.red} ${error.message}`);
+
+    if (error.stack) console.error(error.stack.gray);
   }
 };
